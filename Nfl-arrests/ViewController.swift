41148ab9b7635677e99d9541e4d126225086af36
Kelsey Kjeldsen <kelsey.kjeldsen@ipic.com>
2019-01-31 17:48:45 -0500

    Initial Commit

---
+++ Nfl-arrests/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Nfl-arrests
+//
+//  Created by Kelsey Kjeldsen on 1/31/19.
+//  Copyright © 2019 Kelsey Kjeldsen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
